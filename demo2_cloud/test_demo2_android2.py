0ce546603a761d093f339e1bf54cd20f5df0eafe
balaji-githubstore <balaji0017@gmail.com>
2023-03-22 12:03:56 +0530

    Sign up

--- demo2_cloud/test_demo2_android2.py
+++ demo2_cloud/test_demo2_android2.py
@@ -35,15 +35,40 @@ class TestAndroidDeviceCloud(AppiumConfig):
 
     def test_invalid_sign_up_email_test(self):
         self.driver.find_element(AppiumBy.XPATH, "//android.widget.TextView[@text='Dismiss']").click()
-        #click on setting icon
-        #click on sign in
-        #click on sign up with email
-        #send firstnamea as john
-        #send lastname as peter
-        #send birthday Aug 20, 1995
-        #send password as welcome123
-        #send email as test123
-        #click on create
-        #assert the error message of mail id
+        # click on setting icon
+        self.driver.find_element(AppiumBy.XPATH, "//android.widget.ImageView[@content-desc='Settings']").click()
+        # click on sign in
+        self.driver.find_element(AppiumBy.XPATH, "//android.widget.TextView[@text='Sign in']").click()
+        # click on sign up with email
+        self.driver.find_element(AppiumBy.XPATH, "//*[contains(@text,'email')]").click()
 
+        # send firstnamea as john -
+        self.driver.find_element(AppiumBy.XPATH, "//*[@content-desc='First name']").send_keys("bala")
+        # send lastname as peter -
+        self.driver.find_element(AppiumBy.XPATH, "//*[@content-desc='Last name']").send_keys("bala")
 
+        # send birthday Aug 20, 1995 - Birthday
+        # self.driver.find_element(AppiumBy.XPATH, "//*[@text='Birthday']").send_keys("August 20, 1995")
+        self.driver.find_element(AppiumBy.XPATH, "//*[@text='Birthday']").click()
+
+        #choose Aug
+        self.driver.find_element(AppiumBy.XPATH, "//*[@resource-id='android:id/numberpicker_input']").click()
+        self.driver.find_element(AppiumBy.XPATH, "//*[@resource-id='android:id/numberpicker_input']").clear()
+        self.driver.find_element(AppiumBy.XPATH, "//*[@resource-id='android:id/numberpicker_input']").send_keys("Aug")
+
+        #choose 20
+        self.driver.find_element(AppiumBy.XPATH, "(//*[@resource-id='android:id/numberpicker_input'])[2]").click()
+        self.driver.find_element(AppiumBy.XPATH, "(//*[@resource-id='android:id/numberpicker_input'])[2]").clear()
+        self.driver.find_element(AppiumBy.XPATH, "(//*[@resource-id='android:id/numberpicker_input'])[2]").send_keys("20")
+
+        #choose 1995
+        self.driver.find_element(AppiumBy.XPATH, "(//*[@resource-id='android:id/numberpicker_input'])[3]").click()
+        self.driver.find_element(AppiumBy.XPATH, "(//*[@resource-id='android:id/numberpicker_input'])[3]").clear()
+        self.driver.find_element(AppiumBy.XPATH, "(//*[@resource-id='android:id/numberpicker_input'])[3]").send_keys("1995")
+
+        self.driver.find_element(AppiumBy.XPATH, "//*[@text='OK']").click()
+
+        # send password as welcome123
+        # send email as test123
+        # click on create
+        # assert the error message of mail id
