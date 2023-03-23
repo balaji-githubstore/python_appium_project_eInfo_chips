0a181047ea6c6c3e40347196ea3b591844c7ad26
balaji-githubstore <balaji0017@gmail.com>
2023-03-23 12:37:43 +0530

    Scroll using mobile commands done as of now

--- demo1_android/demo5_native_app_swipe.py
+++ demo1_android/demo5_native_app_swipe.py
@@ -1,8 +1,12 @@
+import time
+
 import pytest
 from appium import webdriver
 from appium.webdriver.common.appiumby import AppiumBy
 
 """Automation using ANDROID_UIAUTOMATOR"""
+
+
 class AppiumConfig:
     @pytest.fixture(scope="function", autouse=True)
     def handle_app_launch(self):
@@ -16,15 +20,32 @@ class AppiumConfig:
         yield
         self.driver.quit()
 
+
 class TestArts(AppiumConfig):
     def test_the_himalayas_topics(self):
-        if len(self.driver.find_elements(AppiumBy.XPATH,"//android.widget.TextView[@text='Dismiss']"))>0:
+        if len(self.driver.find_elements(AppiumBy.XPATH, "//android.widget.TextView[@text='Dismiss']")) > 0:
             self.driver.find_element(AppiumBy.XPATH, "//android.widget.TextView[@text='Dismiss']").click()
 
-        #id is resource-id only
-        self.driver.find_element(AppiumBy.ID,"org.khanacademy.android:id/tab_bar_button_search").click()
+        # id is resource-id only
+        self.driver.find_element(AppiumBy.ID, "org.khanacademy.android:id/tab_bar_button_search").click()
 
-        #ACCESSIBILITY_ID is content-desc in android
+        # ACCESSIBILITY_ID is content-desc in android
         # self.driver.find_element(AppiumBy.ACCESSIBILITY_ID,"Search tab").click()
 
         self.driver.find_element(AppiumBy.XPATH, "//android.widget.TextView[@text='Arts and humanities']").click()
+
+        time.sleep(5)
+        # swipe until //android.widget.TextView[@text='Art of Asia'] presence
+        #  self.driver.swipe(902, 1174,924, 794,1)
+
+        #scroll to art of asia and click
+        para_dic = {"strategy": AppiumBy.ANDROID_UIAUTOMATOR, "selector": 'UiSelector().text("Art of Asia")'}
+        self.driver.execute_script("mobile: scroll",para_dic)
+
+        self.driver.find_element(AppiumBy.XPATH, "//android.widget.TextView[@text='Art of Asia']").click()
+
+        # scroll to the himalayas and click
+        para_dic = {"strategy": AppiumBy.ANDROID_UIAUTOMATOR, "selector": 'UiSelector().textContains("Himala")'}
+        self.driver.execute_script("mobile: scroll", para_dic)
+
+        self.driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR, 'UiSelector().textContains("Himala")').click()
