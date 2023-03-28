e0d6c519cdf181aaec156233fbefa2df2b933ad4
balaji-githubstore <balaji0017@gmail.com>
2023-03-28 11:00:09 +0530

    ios automation cloud - swipe using mobile commands

--- demo3_ios/test_demo3_ios_cloud_sauce.py
+++ demo3_ios/test_demo3_ios_cloud_sauce.py
@@ -1,6 +1,7 @@
 import pytest
 from appium import webdriver
 from appium.webdriver.common.appiumby import AppiumBy
+from assertpy import assert_that
 
 
 class AppiumIosConfig:
@@ -29,7 +30,63 @@ class AppiumIosConfig:
 
 
 class TestSampleApp(AppiumIosConfig):
-    def test_text_box(self):
-# 3.	Enter username as john
-# 4.	Enter password as john123
-# 5.	Get the error message shown and assert it
+    def test_invalid_login(self):
+        self.driver.find_element(AppiumBy.NAME, "test-Username").send_keys("admin")
+        self.driver.find_element(AppiumBy.XPATH, "//XCUIElementTypeSecureTextField[@name='test-Password']").send_keys(
+            "admin123")
+        self.driver.find_element(AppiumBy.IOS_PREDICATE, "name=='test-LOGIN'").click()
+        actual_error = self.driver.find_element(AppiumBy.XPATH,
+                                                "//XCUIElementTypeStaticText[contains(@name,'not match')]").text
+        assert_that(actual_error).contains("Username and password do not match")
+
+    def test_add_to_cart(self):
+        self.driver.find_element(AppiumBy.NAME, "test-Username").send_keys("standard_user")
+        self.driver.find_element(AppiumBy.XPATH, "//XCUIElementTypeSecureTextField[@name='test-Password']").send_keys(
+            "secret_sauce")
+        self.driver.find_element(AppiumBy.IOS_PREDICATE, "name=='test-LOGIN'").click()
+
+        # add to cart 4 items
+        self.driver.find_element(AppiumBy.XPATH, "//XCUIElementTypeOther[@name='test-ADD TO CART']").click()
+        self.driver.find_element(AppiumBy.XPATH, "//XCUIElementTypeOther[@name='test-ADD TO CART']").click()
+        self.driver.find_element(AppiumBy.XPATH, "//XCUIElementTypeOther[@name='test-ADD TO CART']").click()
+        self.driver.find_element(AppiumBy.XPATH, "//XCUIElementTypeOther[@name='test-ADD TO CART']").click()
+
+        self.driver.find_element(AppiumBy.XPATH, "//XCUIElementTypeOther[@name='test-Cart']").click()
+        # swipe and click on checkout
+        print(len(self.driver.find_elements(AppiumBy.XPATH, "//*[@name='test-CHECKOUT']")))
+        print(self.driver.find_element(AppiumBy.XPATH, "//*[@name='test-CHECKOUT']").is_displayed())
+
+        size_dic = self.driver.get_window_size()
+        x1 = size_dic['width'] * (50 / 100)
+        y1 = size_dic['height'] * (75 / 100)
+
+        x2 = size_dic['width'] * (50 / 100)
+        y2 = size_dic['height'] * (25 / 100)
+
+        while not self.driver.find_element(AppiumBy.XPATH, "//*[@name='test-CHECKOUT']").is_displayed():
+            self.driver.swipe(x1, y1, x2, y2, 1000)
+
+        self.driver.find_element(AppiumBy.XPATH, "//*[@name='test-CHECKOUT']").click()
+
+    def test_add_to_cart_mobile_command(self):
+        self.driver.find_element(AppiumBy.NAME, "test-Username").send_keys("standard_user")
+        self.driver.find_element(AppiumBy.XPATH, "//XCUIElementTypeSecureTextField[@name='test-Password']").send_keys(
+            "secret_sauce")
+        self.driver.find_element(AppiumBy.IOS_PREDICATE, "name=='test-LOGIN'").click()
+
+        # add to cart 4 items
+        self.driver.find_element(AppiumBy.XPATH, "//XCUIElementTypeOther[@name='test-ADD TO CART']").click()
+        self.driver.find_element(AppiumBy.XPATH, "//XCUIElementTypeOther[@name='test-ADD TO CART']").click()
+        self.driver.find_element(AppiumBy.XPATH, "//XCUIElementTypeOther[@name='test-ADD TO CART']").click()
+        self.driver.find_element(AppiumBy.XPATH, "//XCUIElementTypeOther[@name='test-ADD TO CART']").click()
+
+        self.driver.find_element(AppiumBy.XPATH, "//XCUIElementTypeOther[@name='test-Cart']").click()
+        # swipe and click on checkout
+        print(len(self.driver.find_elements(AppiumBy.XPATH, "//*[@name='test-CHECKOUT']")))
+        print(self.driver.find_element(AppiumBy.XPATH, "//*[@name='test-CHECKOUT']").is_displayed())
+
+        # swipe based on visiblity
+        while not self.driver.find_element(AppiumBy.XPATH, "//*[@name='test-CHECKOUT']").is_displayed():
+            self.driver.execute_script("mobile: scroll", {"direction": "down"})
+
+        self.driver.find_element(AppiumBy.XPATH, "//*[@name='test-CHECKOUT']").click()
